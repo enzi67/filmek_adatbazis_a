fe7d9689e2d822b39f2859238bc836134f22ce22
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2026-01-08 13:58:07 +0100

    gyakorlas sql

---
+++ gyakorlo.sql
@@ -0,0 +1,62 @@
+--Filmek adatbázis - gyakorló feladatok
+-- Alapvető SELECT lekérdezések
+
+-- 1. feladat
+-- Jelenítsd meg az összes film címét és rendezőjét!
+
+
+-- 2. feladat
+-- Listázd ki az összes színész nevét és nemzetiségét!
+
+
+-- 3. feladat
+-- Mutasd meg az összes film teljes információit (minden oszlop)!
+
+
+-- Szűrés WHERE feltétellel
+
+-- 4. feladat
+-- Keresd meg azokat a filmeket, amelyek 2000 után jelentek meg!
+
+
+-- 5. feladat
+-- Listázd ki azokat a filmeket, amelyek hossza 120 percnél hosszabb!
+
+
+-- 6. feladat
+-- Mutasd meg azokat a színészeket, akik aktívak (aktiv = 'igen')!
+
+
+-- 7. feladat
+-- Keresd meg azokat a filmeket, amelyek értékelése 8.0 felett van!
+
+
+-- 8. feladat
+-- Listázd ki azokat a filmeket, amelyek korhatára 16 vagy 18 év!
+
+
+-- Rendezés ORDER BY használatával
+
+-- 9. feladat
+-- Rendezd az összes filmet megjelenési év szerint növekvő sorrendben!
+
+
+-- 10. feladat
+-- Rendezd a filmeket értékelés szerint csökkenő sorrendben!
+
+
+-- 11. feladat
+-- Listázd ki a színészeket születési dátum szerint növekvő sorrendben!
+
+
+-- Összetett feltételek
+
+-- 12. feladat
+-- Keresd meg azokat a filmeket, amelyek 2010 és 2020 között jelentek meg ÉS értékelésük 7.5 felett van!
+
+-- 13. feladat
+-- Mutasd meg azokat a filmeket, amelyek hossza 90 és 120 perc között van!
+
+
+-- 14. feladat
+-- Listázd ki azokat a színészeket, akik nem magyar nemzetiségűek ÉS aktívak!
