cbbea988c0da45f64c1f17bfefee584b211285fb
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2026-03-26 13:46:16 +0100

    feladatok megoldasa

--- gyakorlo_0326.sql
+++ gyakorlo_0326.sql
@@ -10,19 +10,25 @@ from filmek;
 
 --3. Mennyi az összes film hossza együtt?
 --Írj lekérdezést, ami összeadja az összes film hosszát percben!
-
+select sum(hossz) as osszes_hossz
+from filmek;
 
 --4. Mennyi az átlagos filmhossz?
 --Írj lekérdezést, ami az átlagos hossz értéket számolja ki!
-
+select avg(hossz) as avg_hossz
+from filmek;
 
 --5. Mennyi a legjobb (legmagasabb) értékelés?
 --Írj lekérdezést, ami a ertekeles oszlop legnagyobb értékét adja vissza!
-
+select max(ertekeles) as max_ertekeles
+from filmek;
 
 --6. Mennyi a filmek átlagos értékelése?
 --Írj lekérdezést, ami az ertekeles oszlop átlagát számolja ki!
-
+select avg(ertekeles) as avg_ert
+from filmek,
 
 --7. Rendezők száma
 --Írj lekérdezést, ami megadja, hány különböző rendező szerepel a táblában!
+select count(distinct rendezo) as dis_rendezo
+from filmek;
