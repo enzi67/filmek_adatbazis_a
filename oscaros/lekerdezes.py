525f24c67b0e5690bb20c4371ef4be3804854a54
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2026-05-21 14:14:25 +0200

    mmm...

--- oscaros/lekerdezes.py
+++ oscaros/lekerdezes.py
@@ -2,13 +2,13 @@ import mysql.connector
 
 mydb = mysql.connector.connect(user='root', password='mysql',
                               host='127.0.0.1',
-                              database='oscar')
+                              database='oscar_dijas_filmek')
 # cnx.close()
 
 mycursor = mydb.cursor()
 
 # mycursor.execute('use oscar')
-mycursor.execute('SELECT cim, ev FROM film WHERE nyert = 1 ORDER BY ev ASC;')
+# mycursor.execute('SELECT cim, ev FROM film WHERE nyert = 1 ORDER BY ev ASC;')
 # print(film)
 
 filmek = mycursor.fetchall()
@@ -16,7 +16,10 @@ for film in filmek:
     print(film)
 
 
-mycursor.execute('SELECT * FROM film WHERE ev BETWEEN 1939 AND 1945;')
+mycursor.execute("""SELECT * FROM film
+                WHERE ev BETWEEN 1939 AND 1945
+                AND YEAR(bemutato) BETWEEN 1939 AND 1945;""")
+
 filmek = mycursor.fetchall()
 for film in filmek:
     print(film)
