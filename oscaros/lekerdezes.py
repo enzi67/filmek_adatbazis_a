d2367c8325d3962e64c8d64c346c18b53791f105
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2026-05-21 13:21:31 +0200

    uuuuuuuuuuuhhhhhhhhhhhhh

--- oscaros/lekerdezes.py
+++ oscaros/lekerdezes.py
@@ -1,6 +1,16 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='yes',
+cnx = mysql.connector.connect(user='root', password='mysql',
                               host='127.0.0.1',
-                              database='employees')
-cnx.close()
+                              database='oscar')
+# cnx.close()
+
+mycursor = mydb.cursor()
+
+mycursor.execute('use oscar')
+mycursor.execute('select * from film;')
+filmek = mycursor.fetchall()
+# print(film)
+
+for cim in film:
+    print(cim)
